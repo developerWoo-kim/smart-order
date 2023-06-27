6f0970f906c2328b809889c32535ab6136981167
gunwookim <gunwo151@gmail.com>
2023-06-28 01:39:14 +0900

    * 장바구니 상품 엔티티  - 동일한 옵션이 있는지 확인하는 편의 메소드 추가

--- src/main/java/gwkim/smartorder/order/domain/CartItem.java
+++ src/main/java/gwkim/smartorder/order/domain/CartItem.java
@@ -11,7 +11,9 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static javax.persistence.FetchType.*;
 
@@ -56,6 +58,17 @@ public class CartItem {
         cartOption.addCartItem(this);
     }
 
+    //== 편의 메서드 ==//
+    /**
+     * 동일한 상품과 옵션이 있을 경우 카운트 추가 메소드
+     * @param addItemCount
+     * @param addItemPrice
+     */
+    public void addCartItemCount(int addItemCount, int addItemPrice) {
+        this.count += addItemCount;
+        this.cartItemPrice += addItemPrice;
+    }
+
     /**
      * 옵션의 가격을 더하는 편의 메소드
      * @param optionPrice
@@ -69,6 +82,24 @@ public class CartItem {
      */
     public void culCartItemPrice() {
         this.cartItemPrice *= count;
+    }
+
+    /**
+     * 동일한 옵션이 있는지 확인하는 메서드
+     * @param optionDetails
+     * @return
+     */
+    public boolean hasSameOptions(OptionDetail... optionDetails) {
+        List<Long> cartOptionIds = this.getCartOptions().stream()
+        .map(cartOption -> cartOption.getOptionDetail().getId())
+        .sorted()
+        .collect(Collectors.toList());
+
+        List<Long> sortedOptionDetailIds = Arrays.stream(optionDetails)
+                .map(OptionDetail::getId)
+                .sorted()
+                .collect(Collectors.toList());
+        return cartOptionIds.equals(sortedOptionDetailIds);
     }
 
 
