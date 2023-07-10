d270405072fb01df0a38d0f283b4100ffa2fb22c
gunwookim <gunwo151@gmail.com>
2023-07-10 20:43:07 +0900

    * 주문 테스트

---
+++ src/test/java/gwkim/smartorder/order/OrderServiceTest.java
@@ -0,0 +1,16 @@
+package gwkim.smartorder.order;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+public class OrderServiceTest {
+
+    @Test
+    @DisplayName("장바구니에서 주문상품으로 넘어가는 테스트")
+    public void 장바구니에서_주문상품으로_넘어가는_테스트() throws Exception {
+
+    }
+
+    
+}
